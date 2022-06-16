509ed7cfb91c4acfd53635635a65fa1aa32704c4
Suuyshi <selsherbiny@voyancemed.com>
2022-06-16 15:29:23 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Modal from "react-modal";
+
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__title navbar__item">Notezy</div>
+    </header>
+  );
+}
